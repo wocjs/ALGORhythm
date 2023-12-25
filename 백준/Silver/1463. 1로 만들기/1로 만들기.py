2a4132675c817f93410d8a501387a1b10dbf69b0
선천적 베짱이 <90077061+wocjs@users.noreply.github.com>
2023-12-25 14:36:48 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 118408 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+n = int(input())
+dp = [0] * (n + 1)
+
+for i in range(2, n + 1):
+    # 현재의 수에서 1을 빼는 경우
+    dp[i] = dp[i - 1] + 1
+
+    # 현재의 수가 3으로 나눠 떨어지는 경우
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    # 현재의 수가 2로 나눠 떨어지는 경우
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+print(dp[n])
