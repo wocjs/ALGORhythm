9e7ad447b7afca0d474703f7800c563308072337
선천적 베짱이 <90077061+wocjs@users.noreply.github.com>
2024-01-25 00:14:20 +0900

    [Gold IV] Title: Σ, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.py
+++ 백준/Gold/13172. Σ/Σ.py
@@ -1,21 +1,21 @@
-
-def ans(n, s):
-    return s*mul(n, X-2)%X
-
+# B13172
+import sys
+input  = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 def mul(b, t):
     if t == 1:
-        return b%X
-    if t%2 == 0:
-        tmp = mul(b, t//2)
-        return (tmp*tmp)%X
+        return b % X
+    if t % 2 == 0:
+        tmp = mul(b, t//2)  # 여기서 logN으로 준다!
+        return (tmp*tmp) % X
     else:
-        return b*mul(b, t-1)%X
+        return b*mul(b, t-1) % X    # 이건 1에 수렴이라 신경 ㄴㄴ
 
 X = 1000000007
 m = int(input())
 sm = 0
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    sm += ans(a, b) % X
-print(sm%X)
+    b, a = map(int, input().split())
+    sm += a*mul(b, X-2) % X
+print(sm % X)
