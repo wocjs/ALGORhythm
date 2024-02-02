dd285ec225a239b0be584db9132880d99cc3109e
선천적 베짱이 <90077061+wocjs@users.noreply.github.com>
2024-02-02 10:08:59 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 540 ms, Memory: 134284 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,38 @@
+
+import heapq
+
+INF = 10 ** 9
+n = int(input())
+e = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append([c, b])
+st, en = map(int, input().split())
+
+dist = [INF] * (n + 1)
+dist[st] = 0
+prev = [0] * (n + 1)
+q = [[0, st]]
+while q:
+    val, now = heapq.heappop(q)
+    # pruning : 현재 처리중인 간선의 가중치 > 이제까지 발견된 경로
+    if val > dist[now]:continue
+    # 인접한 노드들 하나씩 확인
+    for nVal, nxt in graph[now]:
+        # 현재까지 가중치 + 다음까지 가는 가중치 < 다음에 저장된 가중치
+        if dist[nxt] > val+nVal:
+            dist[nxt] = val+nVal
+            heapq.heappush(q, [dist[nxt], nxt])
+            # 갱신이 되는 타이밍에 현재 -> 다음도 이음
+            prev[nxt] = now
+
+print(dist[en])
+path = [en]
+now = en
+while now != st:
+    now = prev[now]
+    path.append(now)
+print(len(path))
+path.reverse()
+print(*path)
