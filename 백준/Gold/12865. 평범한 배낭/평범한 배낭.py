82b981464a834363cc5f6b7856a91f0c50090ebb
선천적 베짱이 <90077061+wocjs@users.noreply.github.com>
2024-02-19 16:39:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 376 ms, Memory: 189804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,18 @@
+# B12865
 import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
-arr = [[0, 0]]
+lst = [[0, 0]]
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
-dp = [[0] * (k + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        weight, value = arr[i]
-        if j < weight:
+    w, v = map(int, input().split())
+    lst.append([w, v])
+dp = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        weight, value = lst[i]
+        if weight > j:
             dp[i][j] = dp[i-1][j]
         else:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
-
 print(dp[n][k])
