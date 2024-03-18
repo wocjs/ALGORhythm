a31ef87408a460ecd386e5dca01eed905676f14f
suineG.H <90077061+wocjs@users.noreply.github.com>
2024-03-19 00:28:46 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 136 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,11 @@
+t = int(input())
+arr = [0] * 1000001
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+arr[4] = 7
+for i in range(5, 1000001):
+    arr[i] = (arr[i - 1] + arr[i - 2] + arr[i - 3]) % 1000000009
+for _ in range(t):
+    n = int(input())
+    print(arr[n])
