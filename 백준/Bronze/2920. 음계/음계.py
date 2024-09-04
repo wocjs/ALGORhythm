31f6fffe32933eeacaa82855c401a2eba5418b4b
suineG.H <90077061+wocjs@users.noreply.github.com>
2024-09-04 23:32:32 +0900

    tmp commit 2920.py

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+a = list(map(int, input().split()))
+ 
+ if a == sorted(a):
+     print('ascending')
+     elif a == sorted(a, reverse=True):
+         print('descending')
+         else:
+             print('mixed')
+              
+              
