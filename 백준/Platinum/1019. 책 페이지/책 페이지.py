0bec282008a4ab3a2992da68b5b16a67e1b84dba
suineG.H <90077061+wocjs@users.noreply.github.com>
2024-11-01 13:43:54 +0900

    [Platinum V] Title: 책 페이지, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/1019. 책 페이지/책 페이지.py
@@ -0,0 +1,24 @@
+
+def make_nine(N):
+    while N % 10 != 9:
+        for i in str(N):
+            arr[int(i)] += num
+        N -= 1
+    return N
+
+
+N = int(input())
+arr = [0] * 10
+num = 1
+while N > 0:
+    N = make_nine(N)
+    if N < 10:
+        for i in range(N+1):
+            arr[i] += num
+    else:
+        for i in range(10):
+            arr[i] += (N//10 + 1) * num
+    arr[0] -= num
+    num *= 10
+    N //= 10
+print(*arr)
