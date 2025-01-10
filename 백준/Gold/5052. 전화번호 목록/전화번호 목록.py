fa08c5da121726183374680ec3322d406157bd63
suineG.H <90077061+wocjs@users.noreply.github.com>
2025-01-10 11:10:16 +0900

    [Gold IV] Title: 전화번호 목록, Time: 4476 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,16 @@
+def find(_nms):
+    _nms.sort()
+    for i in range(len(_nms) - 1):
+        if _nms[i] in _nms[i + 1][0:len(_nms[i])]:
+            print("NO")
+            return
+    print("YES")
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    nums = []
+    for _ in range(n):
+        nums.append(input())
+    find(nums)
